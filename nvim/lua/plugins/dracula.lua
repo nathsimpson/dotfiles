9150de7cd1edade99db22b11d01b8815cc91c015
Nathan Simpson <hello@nathansimpson.design>
2025-06-03 19:54:27 +1000

    Tweak neovim config

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -3,7 +3,7 @@ return {
     "dracula/vim",
     name = "dracula",
     config = function()
-      vim.cmd.colorscheme "dracula"
-    end
-  }
+      vim.cmd.colorscheme("dracula")
+    end,
+  },
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
-return { 
+return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
