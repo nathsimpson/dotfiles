a423625b9ea457a2403d2082b1724de13fdd23d1
Nathan Simpson <hello@nathansimpson.design>
2025-10-07 08:45:31 +1100

    Add astro nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,8 @@
-require("nath.lazy")
+-- Ensures lazy.nvim is installed and in runtime path
+require "lazy_init"
+
+-- Load configuration
+-- require("nath.lazy")
+
+require "astro.lazy_setup"
+require "astro.polish"
---
+++ nvim/.config/nvim/lua/astro/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "typescript",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
